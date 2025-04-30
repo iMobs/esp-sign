964ff34929ce5a82a797553e7fe5114324641192
Ian Mobley <ianmobley@gmail.com>
2025-04-29 18:26:22 -0700

    refactor: move led buffer size to const

--- src/leds.rs
+++ src/leds.rs
@@ -5,23 +5,31 @@ use esp_hal::{
     time::Rate,
     Blocking,
 };
-use esp_hal_smartled::{smartLedBuffer, SmartLedsAdapter};
+use esp_hal_smartled::SmartLedsAdapter;
 use smart_leds::{brightness, gamma, SmartLedsWrite};
 
-const LED_COUNT: usize = 1; // TODO: Change this to the number of LEDs you have
+// TODO: Change this to the right number of LEDs
+const LED_COUNT: usize = 1;
+// Copied from smartLedBuffer! macro
+// The size we're assigning here is calculated as following
+//  (
+//   Nr. of LEDs
+//   * channels (r,g,b -> 3)
+//   * pulses per channel 8)
+//  ) + 1 additional pulse for the end delimiter
+const LED_BUFFER_SIZE: usize = LED_COUNT * 24 + 1;
 
 pub async fn init_leds(rmt: esp_hal::peripherals::RMT, pin: AnyPin, spawner: &Spawner) {
     let rmt = Rmt::new(rmt, Rate::from_mhz(80)).unwrap();
 
-    // Annoying that the const LED_COUNT is not available in the macro
-    let buffer = smartLedBuffer!(1);
+    let buffer = [0; LED_BUFFER_SIZE];
     let neopixel = SmartLedsAdapter::new(rmt.channel0, pin, buffer);
 
     spawner.must_spawn(led_task(neopixel));
 }
 
 #[embassy_executor::task]
-async fn led_task(mut neopixel: SmartLedsAdapter<Channel<Blocking, 0>, 25>) {
+async fn led_task(mut neopixel: SmartLedsAdapter<Channel<Blocking, 0>, LED_BUFFER_SIZE>) {
     loop {
         let rgb = crate::RGB_CHANNEL.receive().await;
         let data = [rgb; LED_COUNT];
