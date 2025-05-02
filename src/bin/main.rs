6ef659a496d93f1e72cfa6f21e2750d531318a75
Ian Mobley <ianmobley@gmail.com>
2025-05-01 18:34:25 -0700

    Revert "chore: unused async"
    This reverts commit 33d794b5f72c89d4eeb7f9be9332a59191272132.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -39,6 +39,6 @@ async fn main(spawner: Spawner) {
 
     let stack = esp_sign::wifi::init_wifi(init, peripherals.WIFI, rng, &spawner).await;
 
-    esp_sign::web::init_web(stack, &spawner);
-    esp_sign::leds::init_leds(peripherals.RMT, peripherals.GPIO2.degrade(), &spawner);
+    esp_sign::web::init_web(stack, &spawner).await;
+    esp_sign::leds::init_leds(peripherals.RMT, peripherals.GPIO2.degrade(), &spawner).await;
 }
