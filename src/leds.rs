df6a06058990f53c3f59e69b2916b12645be7bbd
Ian Mobley <ianmobley@gmail.com>
2025-08-10 10:05:43 -0700

    refactor: async leds

--- src/leds.rs
+++ src/leds.rs
@@ -4,44 +4,36 @@ use esp_hal::{
     rmt::{ConstChannelAccess, Rmt, Tx},
     time::Rate,
 };
-use esp_hal_smartled::SmartLedsAdapter;
+use esp_hal_smartled::{buffer_size_async, SmartLedsAdapterAsync};
 use rgb::RGB8;
-use smart_leds::{brightness, gamma, SmartLedsWrite};
+use smart_leds::{brightness, gamma, SmartLedsWriteAsync};
 
 // TODO: Change this to the right number of LEDs
 const LED_COUNT: usize = 10;
-// Copied from smartLedBuffer! macro
-// The size we're assigning here is calculated as following
-//  (
-//   Nr. of LEDs
-//   * channels (r,g,b -> 3)
-//   * pulses per channel 8)
-//  ) + 1 additional pulse for the end delimiter
-const LED_BUFFER_SIZE: usize = LED_COUNT * 24 + 1;
+const LED_BUFFER_SIZE: usize = buffer_size_async(LED_COUNT);
 
 pub async fn init_leds(rmt: esp_hal::peripherals::RMT<'_>, pin: AnyPin<'_>, spawner: &Spawner) {
-    let rmt = Rmt::new(rmt, Rate::from_mhz(80)).unwrap();
+    let rmt = Rmt::new(rmt, Rate::from_mhz(80)).unwrap().into_async();
 
     let buffer = [0; LED_BUFFER_SIZE];
-    let neopixel = SmartLedsAdapter::new(rmt.channel0, pin, buffer);
+    let neopixel = SmartLedsAdapterAsync::new(rmt.channel0, pin, buffer);
 
     spawner.must_spawn(led_task(neopixel));
 }
 
-type Neopixel = SmartLedsAdapter<ConstChannelAccess<Tx, 0>, LED_BUFFER_SIZE>;
+type Neopixel = SmartLedsAdapterAsync<ConstChannelAccess<Tx, 0>, LED_BUFFER_SIZE>;
 
 #[embassy_executor::task]
 async fn led_task(mut neopixel: Neopixel) {
     let mut data = [RGB8::new(0xD1, 0xDD, 0xF1); LED_COUNT];
-    neopixel
-        .write(brightness(gamma(data.into_iter()), 0xFF))
-        .unwrap();
 
     loop {
-        let rgb = crate::RGB_CHANNEL.receive().await;
-        data = [rgb; LED_COUNT];
         neopixel
             .write(brightness(gamma(data.into_iter()), 0xFF))
+            .await
             .unwrap();
+
+        let rgb = crate::RGB_CHANNEL.receive().await;
+        data = [rgb; LED_COUNT];
     }
 }
