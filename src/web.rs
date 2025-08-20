51d42b10c23226a55d1bb390a71d7000741d51c0
Ian Mobley <ianmobley@gmail.com>
2025-08-20 14:40:25 -0700

    chore: format

--- src/web.rs
+++ src/web.rs
@@ -1,11 +1,6 @@
 use crate::WEB_TASK_POOL_SIZE;
 use embassy_time::Duration;
-use picoserve::{
-    extract::Json,
-    make_static,
-    routing::get_service,
-    AppBuilder, AppRouter, Router,
-};
+use picoserve::{extract::Json, make_static, routing::get_service, AppBuilder, AppRouter, Router};
 use rgb::ComponentSlice;
 
 pub struct Application;
